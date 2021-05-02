d3930678ae3176175be2f2882e3ca3a33a1e837b
DevrajDC <devrajchatribin9978@gmail.com>
2021-05-02 16:38:45 +0530

    added covin branding

---
+++ README.md
@@ -0,0 +1,66 @@
+# IndiCov | A platform for all your covid needs. 
+
+![IndiCov](https://user-images.githubusercontent.com/65373279/116795491-fdb16100-aaf2-11eb-8d99-09d0322085d8.png)
+
+IndiCov is a platform that works towards helping patients during this pandemic by informing and constantly updating them of the available resources. People can visit this site and check in the status of the resource availability according to the states or district wise.
+
+## APIs Used
+ 
+| Data                              | URL                                                                             |
+| --------------------------------- | ------------------------------------------------------------------------------- |
+| List of districts with Data       | <https://life-api.coronasafe.network/data/active_district_data.json>         |
+| Ambulance availability            | <https://life-api.coronasafe.network/data/ambulance.json>                       |
+| Helpline Numbers                  | <https://life-api.coronasafe.network/data/helpline.json>                        |
+| Hospitals and Beds                | <https://life-api.coronasafe.network/data/hospital.json>          |
+| Medicine availability             | <https://life-api.coronasafe.network/data/medicine.json>                        |
+| Oxygen availability               | <https://life-api.coronasafe.network/data/oxygen.json>                          |
+
+## Want to help with coding?
+Join our discord and get in touch with several other developers.
+<br>
+<a href="https://discord.gg/druweDMn3s" style="text-decoration:none">
+  <img height="30" src="https://img.shields.io/badge/discord-darkblue.svg?&style=for-the-badge&logo=discord&logoColor=white" />
+</a>
+
+# How you can start contributing to IndiCov?
+
+Types of Contributions:
+1. Code (Front-End)
+2. Manpower (volunteers for verification)
+3. Amplification - Help spread the word on social media
+
+## Ready to start
+We welcome everyone to join in the construction of the project. As a pre requirement, you need to have a preliminary understanding of React , this is a good learning document for [React](https://reactjs.org/docs/getting-started.html).
+
+For basic operation of Git, you can refer to [GitHub's help documentation](https://help.github.com/en/github/using-git)
+
+We are using Material UI for developing the Website , [Material UI Docs](https://material-ui.com/) is an amazing place to get started and refer.
+
+Any kind of Contributions is Accepted.
+
+## Contributing
+[Fork this repository](https://github.com/Design-and-Code/IndiCov) to your own account and then clone it.
+Create a new branch for your changes: `git checkout -b {BRANCH_NAME}`.
+You can either use npm or yarn as your package manager.
+
+
+## Getting Started
+
+First, run the development server:
+
+```
+npm install
+npm run dev
+# or
+yarn dev
+```
+
+Open http://localhost:3000 with your browser to see the result.
+
+You can start editing the page by modifying pages/index.js. The page auto-updates as you edit the file.
+
+# Got stuck ?
+
+- Create new issue to tell us: create [issue](https://github.com/Design-and-Code/IndiCov/issues/new/choose).
+
+- Feel Free to Ask on our [Discord](https://discord.gg/druweDMn3s).
---
+++ src/pages/AboutPage/index.jsx
@@ -0,0 +1,189 @@
+import React from "react";
+
+// import { Container } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+
+import styles from "./style";
+import { Grid, Typography, Hidden, Container } from "@material-ui/core";
+import Tip from "../../components/Tip";
+
+import medicalMask from "../../assets/medical-mask 1.png";
+import hands from "../../assets/washing-hands 1.png";
+import liquidSoap from "../../assets/liquid-soap 1.png";
+import socialDist from "../../assets/social-distancing 1.png";
+import Member from "../../components/Member";
+
+import Ankita from "./assets/Ankita.png";
+import Devraj from "./assets/Devraj 1.png";
+import Jaagrav from "./assets/Jaagrav.png";
+import Shrutika from "./assets/Shrutika 1.png";
+import Trusha from "./assets/Trusha.png";
+import Shridhar from "./assets/Shridhar.png";
+
+const useStyles = makeStyles(styles);
+
+function AboutPage() {
+  const classes = useStyles();
+
+  return (
+    <Grid container className={classes.aboutPage}>
+      <Hidden mdUp>
+        <Grid xs={12}>
+          <Typography variant="h5" className={classes.brandingName}>
+            Cov
+            <Typography
+              variant="h5"
+              className={classes.secondaryColorBrandingName}
+            >
+              in
+            </Typography>
+          </Typography>
+        </Grid>
+      </Hidden>
+
+      {/* Hero Section */}
+      <div className={classes.about_main}>
+        <Typography variant="h4" className={classes.title}>
+          What is{" "}
+          <Typography variant="h4" className={classes.title_underline}>
+            Covin
+          </Typography>{" "}
+          ?
+        </Typography>
+        <Typography variant="caption" gutterBottom className={classes.info}>
+          <strong>Covin</strong> is a platform created by our team to help
+          patients during this pandemic. The coronavirus scare in India has left
+          a tremendous impact on its citizens. There is a shortage of resources.
+          These resources include oxygen, beds, plasma, blood, ventilators,
+          ambulances, vaccines, and other medications.{" "}
+        </Typography>
+        <Typography variant="caption" className={classes.info}>
+          With <strong>IndiCov</strong>, we plan to provide all the help
+          possible to the patient by informing and constantly updating them of
+          the available resources.
+        </Typography>
+      </div>
+
+      {/* Prevention Section */}
+      <Grid container>
+        <Grid item sm={12}>
+          <Typography variant="h4" className={classes.title}>
+            Basic Prevention Methods
+          </Typography>
+        </Grid>
+        <Grid container sm={12} md={6}>
+          <Tip
+            image={medicalMask}
+            title={"Face covers/masks"}
+            description={
+              "Wear a mask in public and ensure it covers your nose and mouth and is secure under the chin."
+            }
+          />
+          <Tip
+            image={hands}
+            title={"Wash Hands frequently"}
+            description={
+              "Wash hands with soap and water for at least 20 seconds and avoid touching your face."
+            }
+          />
+        </Grid>
+        <Grid container xs={12} sm={12} md={6} lg={6} xl={6}>
+          <Tip
+            image={liquidSoap}
+            title={"Use of sanitizers"}
+            description={
+              "Use an alcohol-based sanitizer to prevent the spread of germs."
+            }
+          />
+          <Tip
+            image={socialDist}
+            title={"Physical distancing"}
+            description={
+              "Stay at least 6 feet apart from people who don't live with you, and avoid close contact with sick people."
+            }
+          />
+        </Grid>
+      </Grid>
+
+      {/* Team Section */}
+      <Typography variant="h4" className={classes.title}>
+        Our Team
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom className={classes.info}>
+        Meet the spirited team that worked behind the scenes to establish this
+        platform.
+      </Typography>
+
+      <Member
+        image={Devraj}
+        name="Devraj Chatribin"
+        occ1="Founder & Design Lead"
+        occ2="Web Developer | UI/UX Designer"
+        linkedIn="https://www.linkedin.com/in/devraj-chatribin/"
+        instagram="https://www.instagram.com/cuppycake_001/"
+        github="https://github.com/DevrajDC"
+        website="https://devraj-chatribin.netlify.app/"
+      />
+      <Member
+        image={Ankita}
+        name="Ankita Sahu"
+        occ1="Events & Graphics Lead"
+        occ2="Web Developer | Content Writer"
+        linkedIn="https://www.linkedin.com/in/ankita-sahu-540920201/"
+        github="https://github.com/SAHU-01"
+      />
+      <Member
+        image={Jaagrav}
+        name="Jaagrav Seal"
+        occ1="Development Lead"
+        occ2="Creative Web Developer"
+        linkedIn="https://www.linkedin.com/in/jaagrav/"
+        github="https://github.com/Jaagrav/"
+        website="https://xjaagrav.netlify.app/stories"
+      />
+      <Member
+        image={Shrutika}
+        name="Shrutika Gawas"
+        occ1="Social Media Administrator"
+        occ2="Web Developer"
+        linkedIn="https://www.linkedin.com/in/shrutika-g-40829910b/"
+        instagram="https://www.instagram.com/shrutikagawas/"
+      />
+      <Member
+        image={Trusha}
+        name="Trusha Neogi"
+        occ1="Graphic & Content Lead"
+        occ2="Graphic Designer"
+        linkedIn="https://www.linkedin.com/in/trusha-neogi-6a69bb191/"
+        instagram="https://www.instagram.com/tuuushaa/"
+      />
+      <Member
+        image={Shridhar}
+        name="Shridhar Kamat"
+        occ1="Development Team"
+        occ2="Web Developer"
+        linkedIn="https://www.linkedin.com/in/shridhar-kamat-1015a41bb"
+        github="https://github.com/Shridhar-dev"
+        website="https://thehardwaronzone.com/"
+      />
+
+      {/* Sources Section */}
+      <div className={classes.sources}>
+        <Typography variant="h3" className={classes.title}>
+          Sources
+        </Typography>
+        <Typography variant="subtitle1" className={classes.info}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Typography>
+      </div>
+    </Grid>
+  );
+}
+
+export default AboutPage;
